8053d87fe94d4d8efd67e987eeacc3326fd0775e
Aman <amankrdev28@gmail.com>
2026-07-14 22:43:33 +0530

    refactor: migrate to RouterProvider with root layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import React from "react";
-import Navbar from "./components/Navbar.jsx";
 import Home from "./pages/Home.jsx";
 import Products from "./pages/Products.jsx";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
-import { Routes, Route } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./layout/RootLayout.jsx";
+
+const Router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="products" element={<Products />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="about" element={<About />} />
+    </Route>,
+  ),
+);
 
 const App = () => {
   return (
     <>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+      <RouterProvider router={Router} />;
     </>
   );
 };
